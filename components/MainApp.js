d622da7c4362ae7fd2dcade53a003727087f6868
bacini <bagasp5621@gmail.com>
2021-11-25 08:26:24 +0700

    Create MainApp.js

---
+++ components/MainApp.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import {
+  View,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Modal,
+} from "react-native";
+
+const MainApp = () => {};
+
+const styles = StyleSheet.create({});

54903fc212a2136c6cf679a6c0bdbc6fe26a909b
bacini <bagasp5621@gmail.com>
2021-11-26 12:14:55 +0700

    kategori di home

---
+++ components/Kategori.js
@@ -0,0 +1,76 @@
+import React from "react";
+import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
+
+const Kategori = (props) => {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={() => {}}>
+        <View style={styles.kategori}>
+          <Image
+            style={styles.image}
+            blurRadius={6}
+            source={require("./img/dummy/pakaian.png")}
+          />
+          <View>
+            <Text style={styles.text}>Pakaian</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => {}}>
+        <View style={styles.kategori}>
+          <Image
+            style={styles.image}
+            blurRadius={6}
+            source={require("./img/dummy/aksesoris.png")}
+          />
+          <View>
+            <Text style={styles.text}>Aksesoris</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    top: 15,
+    paddingLeft: 22,
+    paddingRight: 22,
+    elevation: 100,
+  },
+  kategori: {
+    backgroundColor: "#C4C4C4",
+    marginBottom: 15,
+    borderRadius: 8,
+    elevation: 10,
+    shadowColor: "#000",
+    flexDirection: "row",
+  },
+  text: {
+    color: "#444745",
+    textAlignVertical: "center",
+    textAlign: "center",
+    fontFamily: "Roboto",
+    fontSize: 19,
+    width: 207,
+    height: 0,
+    borderBottomWidth: 90,
+    borderLeftWidth: 45,
+    borderTopColor: "transparent",
+    borderRightColor: "transparent",
+    borderBottomColor: "#F9F9F9",
+    borderLeftColor: "transparent",
+    marginLeft: -85,
+    borderTopRightRadius: 8,
+    borderBottomRightRadius: 8,
+  },
+  image: {
+    width: 245,
+    height: 90,
+    borderRadius: 8,
+  },
+});
+
+export default Kategori;
--- components/MainApp.js
+++ components/MainApp.js
@@ -1,23 +1,26 @@
-import React, { useState } from "react";
-import {
-  View,
-  StyleSheet,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  Modal,
-} from "react-native";
+import React from "react";
+import { View, StyleSheet, ScrollView } from "react-native";
 
 import Header from "./Header";
+import Kategori from "./Kategori";
 
 const MainApp = () => {
   return (
     <View>
       <Header />
+      <View style={styles.scrollView}>
+        <ScrollView horizontal>
+          <Kategori />
+        </ScrollView>
+      </View>
     </View>
   );
 };
-
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  scrollView: {
+    top: 80,
+    height: 604,
+  },
+});
 
 export default MainApp;
